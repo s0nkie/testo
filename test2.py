13ae019f9f8635293e095d23e671eef01c12972b
s0nkie <70837429+s0nkie@users.noreply.github.com>
2021-03-20 10:58:01 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,25 +1,11 @@
 a = [i for i in range(11, 80)]
 
-b = [i if i % 3==0 else "$$" for i in a]
-c = 
-
-print(d)
-        
-
-
-# new_items = [x if x % 3 else '$$' for x in a]
-
-
-
-# for j in a:
-#     if j % 3 == 0:
-#         print('$$')
-#     elif j % 5 == 0:
-#         print('@@')
-#     else:
-#         j % 3 == 0 or j % 5 == 0
-#         print('@@$$')
-# 
-# 
-#  
-
+for j in a:
+    if j % 3 == 0 and j % 5 == 0:
+        print('@@$$')
+    elif j % 5 == 0:
+        print('@@')
+    elif j % 3 == 0:
+        print('$$')
+    else:
+        print(j)
